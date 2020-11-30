809193677d7aa68966265ecd6d6a7de412e23cb1
Ivan B <ivan.giv14@gmail.com>
2020-11-30 22:47:03 +0500

    excircise 1

---
+++ ex1.js
@@ -0,0 +1,45 @@
+const xmlString = `<list>
+  <student>
+    <name lang="en">
+      <first>Ivan</first>
+      <second>Ivanov</second>
+    </name>
+    <age>35</age>
+    <prof>teacher</prof>
+  </student>
+  <student>
+    <name lang="ru">
+      <first>Петр</first>
+      <second>Петров</second>
+    </name>
+    <age>58</age>
+    <prof>driver</prof>
+  </student>
+</list>`;
+
+
+function xmlToJSObj(xmlStr) {
+    const parser = new DOMParser()
+    const parsedXML = parser.parseFromString(xmlStr, 'text/xml')
+    const listStudent = parsedXML.querySelector("list")
+    const students = listStudent.querySelectorAll("student")
+
+    let list = []
+    students.forEach(student => {
+        let nameTag = student.querySelector("name")
+        let firstName = nameTag.querySelector("first").textContent
+        let secondName = nameTag.querySelector("second").textContent
+
+        let obj = {
+            name: firstName + ' ' + secondName,
+            age: parseInt(student.querySelector("age").textContent),
+            prof: student.querySelector("prof").textContent,
+            lang: nameTag.getAttribute("lang")
+        }
+
+        list.push(obj)
+    })
+    return list
+}
+
+console.log(xmlToJSObj(xmlString))
